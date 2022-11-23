029be35fed689373b17c5689a1a2ef3694f26bc7
Al Amin Eimon <eimonalamin@gmail.com>
2022-11-24 00:58:29 +0600

    get data by category

--- index.js
+++ index.js
@@ -9,6 +9,59 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.juguzvf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+console.log(uri);
+
+
+async function run() {
+  try {
+    const productCollection = client.db("tirex").collection("services");
+
+    //for get all service
+    app.get("/services", async (req, res) => {
+      const query = {};
+      console.log(req.params);
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+      
+    //get data by kawasaki category
+    app.get("/kawasaki", async (req, res) => {
+      // const query = {}
+      const products = await productCollection
+        .find({ categoryName: "kawasaki" })
+        .toArray();
+      res.send(products);
+    });
+    //get data by ktm category
+    app.get("/ktm", async (req, res) => {
+      // const query = {}
+      const products = await productCollection
+        .find({ categoryName: "ktm" })
+        .toArray();
+      res.send(products);
+    });
+    //get data by yamaha category
+    app.get("/yamaha", async (req, res) => {
+      // const query = {}
+      const products = await productCollection
+        .find({ categoryName: "yamaha" })
+        .toArray();
+      res.send(products);
+    });
+      
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
+
 
 
 app.get("/", (res, req) => {
